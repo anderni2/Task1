88b47c9efe5797ce4756c36b5a492f89af1f6c16
anderni2 <anderni2@students.zhaw.ch>
2024-05-06 15:17:24 +0200

    changes

--- src/routes/appartment/+page.svelte
+++ src/routes/appartment/+page.svelte
@@ -2,6 +2,7 @@
     import { PUBLIC_BASE_URL } from '$env/static/public';
  	import axios from "axios";
 
+
 	let bfs_number_names = [
 		{id: 261, name: `Zürich` },
 		{id: 62, name: `Kloten` },
@@ -128,29 +129,46 @@
         });
 	}
 </script>
-<div class="container text-center">
 
-	<h1>Are you paying to much? The model predicts that you should be paying {price}</h1>
+<div class="container text-center">
+	<h1>
+		Are you paying to much? The model predicts that you should be paying {price}
+	</h1>
 
 	<div class="row justify-content-md-center">
-	  <div class="col col-lg-2">
-		<input type="number" class="form-control" placeholder="area" aria-label="area" bind:value={area}>
-	  </div>
-	  <div class="col col-lg-2">
-		<input type="number" class="form-control" placeholder="rooms" aria-label="rooms" bind:value={rooms}>
-	  </div>
-	  <div class="col col-lg-2">
-
-		<select class="form-select" bind:value={selected}>
-			{#each bfs_number_names as bfs}
-				<option value={bfs}>
-					{bfs.name}
-				</option>
-			{/each}
-		</select>
-	  </div>
-	  <div class="col-md-auto">
-		<button type="button" class="btn btn-primary" on:click={handleSubmit}>price estimation</button>
-	  </div>
+		<div class="col col-lg-2">
+			<input
+				type="number"
+				class="form-control"
+				placeholder="area"
+				aria-label="area"
+				bind:value={area}
+			/>
+		</div>
+		<div class="col col-lg-2">
+			<input
+				type="number"
+				class="form-control"
+				placeholder="rooms"
+				aria-label="rooms"
+				bind:value={rooms}
+			/>
+		</div>
+		<div class="col col-lg-2">
+			<select class="form-select" bind:value={selected}>
+				{#each bfs_number_names as bfs}
+					<option value={bfs}>
+						{bfs.name}
+					</option>
+				{/each}
+			</select>
+		</div>
+		<div class="col-md-auto">
+			<button
+				type="button"
+				class="btn btn-primary"
+				on:click={handleSubmit}>price estimation</button
+			>
+		</div>
 	</div>
-  </div>
+</div>
